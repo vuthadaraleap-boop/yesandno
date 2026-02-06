5c69316b5332b75d19c695510b9eb285994b40a7
vuthadaraleap-boop <daraleapyt@gmail.com>
2026-02-06 13:54:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,15 +54,15 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
+    "Are you sure babyy?",
+    "Really sure babyy??",
     "Are you positive?",
-    "Pookie please...",
+    "Babyyy please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "I will be very sad... babyy",
+    "I will be very very very sad... baby",
+    "Okay fine babyy, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
 
